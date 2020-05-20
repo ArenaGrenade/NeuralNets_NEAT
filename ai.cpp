9503f0c6e04050ea17ff5c5dc9ee7671024cae01
abhishekh2001 <53903855+abhishekh2001@users.noreply.github.com>
2020-05-20 18:06:05 +0530

    Added a full fledged AI system
    Detects whether input is ONE or not a ONE.
    Careful with the powers attached with this advanced intelligence system

---
+++ ai.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n == 1) {
+        cout << "One" << endl;
+    } else {
+        cout << "Not one" << endl;
+    }
+    return 0;
+}
